60ffdbf30019579a7e81a90bba330391e61db169
NguyenHuuVietCuong-UIT <nguyenhuuvietcuong123@gmail.com>
2025-11-18 08:29:36 +0700

    Refactor code structure for improved readability and maintainability

--- assets/js/customheader.js
+++ assets/js/customheader.js
@@ -8,87 +8,93 @@ class CustomHeader extends HTMLElement {
         this.innerHTML = `
             <link rel="stylesheet" href="/assets/css/style.css">
             <link rel="stylesheet" href="/assets/css/SettingTab.css">
+            <link rel="stylesheet" href="/assets/css/header.css">
 
             <header class="header">
-    <nav class="nav">
-        <a href="/index.html" aria-label="Biểu trưng của App Market" class="header__logo-link">
-            <img src="/assets/logo/logo.png" alt="App Market logo" />
-            <span aria-hidden="true">App Market</span>
-        </a>
+                <nav class="nav">
+                    <a href="/index.html" aria-label="Biểu trưng của App Market" class="header__logo-link">
+                        <img src="/assets/logo/logo.png" alt="App Market logo" />
+                        <span aria-hidden="true">App Market</span>
+                    </a>
 
-        <span class="navigation-tabs"><a role="tab"
-                class="nav-tab nav-tab-selected nav-tab--auto-width nav-tab--styled FEsNhd YmRZ5d" aria-selected="true"
-                tabindex="0" id="11" href="/index.html">
-                <span class="nav-tab__inner"><span class="nav-tab__label">Trò chơi</span><span
-                        class="nav-tab__indicator-wrapper nav-tab__indicator-wrapper-selected nav-tab__indicator-border kte1hc"><span
-                            class="nav-tab__indicator nav-tab__indicator-format indicator-length"></span></span></span>
-                <div class="VfPpkd-wJCpie-LhBDec"></div>
-            </a><a role="tab" class="nav-tab nav-tab--auto-width nav-tab--styled FEsNhd YmRZ5d q2wkXd"
-                aria-selected="true" tabindex="-1" id="1" href="/html/app_nav.html">
-                <span class="nav-tab__inner"><span class="nav-tab__label">Ứng dụng</span><span
-                        class="nav-tab__indicator-wrapper nav-tab__indicator-border kte1hc"><span
-                            class="nav-tab__indicator nav-tab__indicator-format indicator-length"></span></span></span>
-                <div class="VfPpkd-wJCpie-LhBDec"></div>
-            </a>
-            <a role="tab" class="nav-tab nav-tab--auto-width nav-tab--styled FEsNhd YmRZ5d dUghbc" aria-selected="false"
-                tabindex="-1" id="13" href="/html/children_nav.html">
-                <span class="nav-tab__inner"><span class="nav-tab__label">Trẻ em</span><span
-                        class="nav-tab__indicator-wrapper nav-tab__indicator-border kte1hc"><span
-                            class="nav-tab__indicator nav-tab__indicator-format indicator-length"></span></span></span>
-                <div class="VfPpkd-wJCpie-LhBDec"></div>
-            </a></span>
+                    <span class="navigation-tabs">
+                        <a role="tab" class="nav-tab nav-tab-selected nav-tab--auto-width nav-tab--styled FEsNhd YmRZ5d" 
+                           aria-selected="true" tabindex="0" id="11" href="/index.html">
+                            <span class="nav-tab__inner">
+                                <span class="nav-tab__label">Trò chơi</span>
+                                <span class="nav-tab__indicator-wrapper nav-tab__indicator-wrapper-selected nav-tab__indicator-border kte1hc">
+                                    <span class="nav-tab__indicator nav-tab__indicator-format indicator-length"></span>
+                                </span>
+                            </span>
+                            <div class="VfPpkd-wJCpie-LhBDec"></div>
+                        </a>
 
-        <div class="header-actions">
-            <!-- search bar -->
-            <div class="search__box" id="search-button">
-                <input class="search__input" id="search-input" type="text" placeholder="Search" />
-                <span class="clear__button" id="clear-button" style="cursor: pointer; display: none">
-                    &times;
-                </span>
-                <div class="suggestions__box" id="suggestions-box" style="display: none"></div>
-            </div>
+                        <a role="tab" class="nav-tab nav-tab--auto-width nav-tab--styled FEsNhd YmRZ5d q2wkXd" 
+                           aria-selected="true" tabindex="-1" id="1" href="/html/app_nav.html">
+                            <span class="nav-tab__inner">
+                                <span class="nav-tab__label">Ứng dụng</span>
+                                <span class="nav-tab__indicator-wrapper nav-tab__indicator-border kte1hc">
+                                    <span class="nav-tab__indicator nav-tab__indicator-format indicator-length"></span>
+                                </span>
+                            </span>
+                            <div class="VfPpkd-wJCpie-LhBDec"></div>
+                        </a>
 
-            <!-- help icon -->
-            <button class="icon-button">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-                    <path
-                        d="M478-240q21 0 35.5-14.5T528-290q0-21-14.5-35.5T478-340q-21 0-35.5 14.5T428-290q0 21 14.5 35.5T478-240Zm-36-154h74q0-33 7.5-52t42.5-52q26-26 41-49.5t15-56.5q0-56-41-86t-97-30q-57 0-92.5 30T342-618l66 26q5-18 22.5-39t53.5-21q32 0 48 17.5t16 38.5q0 20-12 37.5T506-526q-44 39-54 59t-10 73Zm38 314q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
-                </svg>
-            </button>
-            <div class="profile-menu-container">
-                <button class="icon-button icon_container" id="avatar-button">
-                    <img src="https://lh3.googleusercontent.com/a/ACg8ocLy4_GnkIHGe1yeIk6TVmY8FNzNneyNjrQtrPKyFiHhxFSFvg=s32-c-k-cc-mo"
-                        class="icon_inner" />
-                </button>
+                        <a role="tab" class="nav-tab nav-tab--auto-width nav-tab--styled FEsNhd YmRZ5d dUghbc" 
+                           aria-selected="false" tabindex="-1" id="13" href="/html/children_nav.html">
+                            <span class="nav-tab__inner">
+                                <span class="nav-tab__label">Trẻ em</span>
+                                <span class="nav-tab__indicator-wrapper nav-tab__indicator-border kte1hc">
+                                    <span class="nav-tab__indicator nav-tab__indicator-format indicator-length"></span>
+                                </span>
+                            </span>
+                            <div class="VfPpkd-wJCpie-LhBDec"></div>
+                        </a>
+                    </span>
 
-                <div class="profile-dropdown" id="profile-dropdown-menu">
-                    <div class="dropdown-header">
-                        <img src="https://lh3.googleusercontent.com/a/ACg8ocLy4_GnkIHGe1yeIk6TVmY8FNzNneyNjrQtrPKyFiHhxFSFvg=s32-c-k-cc-mo"
-                            class="dropdown-avatar" alt="Avatar" />
-                        <div class="user-info">
-                            <span class="user-name">Tên Của Bạn</span>
-                            <span class="user-email">email@cuaban.com</span>
+                    <div class="header-actions">
+                        <div class="search__box" id="search-button">
+                            <input class="search__input" id="search-input" type="text" placeholder="Search" />
+                            <span class="clear__button" id="clear-button" style="cursor: pointer; display: none">×</span>
+                            <div class="suggestions__box" id="suggestions-box" style="display: none"></div>
+                        </div>
+
+                        <button class="icon-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                                <path d="M478-240q21 0 35.5-14.5T528-290q0-21-14.5-35.5T478-340q-21 0-35.5 14.5T428-290q0 21 14.5 35.5T478-240Zm-36-154h74q0-33 7.5-52t42.5-52q26-26 41-49.5t15-56.5q0-56-41-86t-97-30q-57 0-92.5 30T342-618l66 26q5-18 22.5-39t53.5-21q32 0 48 17.5t16 38.5q0 20-12 37.5T506-526q-44 39-54 59t-10 73Zm38 314q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+                            </svg>
+                        </button>
+
+                        <div class="profile-menu-container">
+                            <button class="icon-button icon_container" id="avatar-button">
+                                <img src="https://lh3.googleusercontent.com/a/ACg8ocLy4_GnkIHGe1yeIk6TVmY8FNzNneyNjrQtrPKyFiHhxFSFvg=s32-c-k-cc-mo" class="icon_inner" alt="Avatar Button"/>
+                            </button>
+
+                            <div class="profile-dropdown" id="profile-dropdown-menu">
+                                <div class="dropdown-header">
+                                    <img src="https://lh3.googleusercontent.com/a/ACg8ocLy4_GnkIHGe1yeIk6TVmY8FNzNneyNjrQtrPKyFiHhxFSFvg=s32-c-k-cc-mo" class="dropdown-avatar" alt="Avatar" />
+                                    <div class="user-info">
+                                        <span class="user-name">Tên Của Bạn</span>
+                                        <span class="user-email">email@cuaban.com</span>
+                                    </div>
+                                </div>
+
+                                <ul class="dropdown-list">
+                                    <li><a href="/html/library.html">Thư viện và thiết bị</a></li>
+                                    <li><a href="/html/payment.html">Thông tin thanh toán và gói thuê bao</a></li>
+                                    <li><a href="/html/setting.html">Cài đặt</a></li>
+                                </ul>
+
+                                <div class="dropdown-divider"></div>
+
+                                <ul class="dropdown-list">
+                                    <li><a href="#">Đăng xuất</a></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-
-                    <ul class="dropdown-list">
-                        <li><a href="/html/library.html">Thư viện và thiết bị</a></li>
-                        <li><a href="/html/payment.html">Thông tin thanh toán và gói thuê bao</a></li>
-                        <li><a href="/html/setting.html">Cài đặt</a></li>
-                    </ul>
-
-                    <div class="dropdown-divider"></div>
-
-                    <ul class="dropdown-list">
-                        <li><a href="#">Đăng xuất</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-        </div>
-    </nav>
-</header>
+                </nav>
+            </header>
         `;
     }
 }
