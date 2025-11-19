8b74a5f5cff45bf9ac4d5a83e39fcca409fd8ec5
Nguyen Hieu <nguyendongochieu@gmail.com>
2025-11-20 02:50:14 +0700

    cập nhật responsive cho ranking list và filter

--- assets/js/components/RankingModal.js
+++ assets/js/components/RankingModal.js
@@ -118,7 +118,10 @@ class RankingModal extends HTMLElement {
             <div class="app-details">
                 <div class="app-name">${item.name}</div>
                 <div class="app-category">${item.category}</div>
-                <div class="app-rating"><span>${item.rating}</span> <span style="font-size:12px">★</span></div>
+                <div class="app-rating"><span>${item.rating}</span> <span><svg fill="currentColor" width="14" height="14" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path></svg></span></div>
             </div>
         </div>`;
     });
